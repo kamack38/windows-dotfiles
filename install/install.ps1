201d45598fbb28e7269411d13ba7a5dc87baa624
kamack38 <64226248+kamack38@users.noreply.github.com>
2021-08-06 23:05:01 +0200

    Fix download

--- install/install.ps1
+++ install/install.ps1
@@ -2,5 +2,6 @@ $DOTFILES = "$HOME\.dotfiles"
 $repo = "https://github.com/kamack38/dotfiles.git"
 
 git clone --bare $repo $DOTFILES
+git --git-dir="$DOTFILES" --work-tree="$HOME" fetch -all
 git --git-dir="$DOTFILES" --work-tree="$HOME" config --local status.showUntrackedFiles no
 git --git-dir="$DOTFILES" --work-tree="$HOME" checkout
