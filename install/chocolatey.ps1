a404b45c942dcdfdae92b5a79f6a8569f9aa9ac4
kamack38 <64226248+kamack38@users.noreply.github.com>
2021-08-08 00:49:54 +0200

    Change env reloading method

--- install/chocolatey.ps1
+++ install/chocolatey.ps1
@@ -60,7 +60,7 @@ Invoke-WebRequest -Uri "https://download01.logi.com/web/ftp/pub/techsupport/gami
 Install-Module oh-my-posh -Scope CurrentUser -Force
 Install-Module posh-git -Scope CurrentUser -Force
 
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+refreshenv
 
 iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/kamack38/dotfiles/main/install/install.ps1'))
 
