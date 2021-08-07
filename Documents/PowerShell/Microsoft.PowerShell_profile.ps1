631def75a49d512456ef5fd6f1b60097a2e39254
kamack38 <64226248+kamack38@users.noreply.github.com>
2021-08-07 16:29:17 +0200

    Update powershell profile

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,3 +2,21 @@ Import-Module oh-my-posh
 Import-Module posh-git
 Set-PoshPrompt -Theme ~/.config/themes/kamack.omp.json
 $env:POSH_GIT_ENABLED = $true
+Set-Alias -Name exp -Value C:\Windows\explorer.exe
+function ln { 
+	param(
+		[switch]$s,
+		
+		[Parameter(Mandatory)]
+		[string]$Path,
+		
+		[Parameter(Mandatory)]
+		[string]$Target
+	)
+	if ($s -eq $true) {
+		New-Item -ItemType SymbolicLink -Path $Path -Target $Target
+	}
+	else {
+		New-Item -ItemType HardLink -Path $Path -Target $Target
+	}
+}
