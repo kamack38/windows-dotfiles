4cea6758ac138acaea4c74de37caf3536e3717d9
kamack38 <64226248+kamack38@users.noreply.github.com>
2021-08-08 01:01:27 +0200

    Fix install

--- install/install.ps1
+++ install/install.ps1
@@ -2,7 +2,7 @@ $DOTFILES = "$HOME\.dotfiles"
 $repo = "https://github.com/kamack38/dotfiles.git"
 
 git clone --bare $repo $DOTFILES
-git --git-dir="$DOTFILES" --work-tree="$HOME" fetch -all
+git --git-dir="$DOTFILES" --work-tree="$HOME" fetch --all
 git --git-dir="$DOTFILES" --work-tree="$HOME" config --local status.showUntrackedFiles no
 git --git-dir="$DOTFILES" --work-tree="$HOME" checkout
 # SIG # Begin signature block
