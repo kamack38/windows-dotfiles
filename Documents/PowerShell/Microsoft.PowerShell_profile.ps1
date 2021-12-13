82b0353ef51b9316510364391dc046e667a7a626
kamack38 <64226248+kamack38@users.noreply.github.com>
2021-12-13 22:36:57 +0100

    🔧 Update VSCode keybinds & posh-theme

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -20,42 +20,45 @@ Set-Alias -Name List-Module -Value Get-InstalledModule
 Set-Alias -Name choco -Value choco.exe
 Set-Alias -Name code -Value code.cmd
 Set-Alias -Name node -Value node.exe
-function rmrf {Remove-Item -Recurse -Force $args}
-function la {Get-ChildItem -Force}
-function lw {Get-ChildItem | Format-Wide}
-function lo {Get-ChildItem | Format-Wide -Column 3}
-function qrcode {curl qrcode.show/$args}
-function .. {Set-Location ../}
-function ... {Set-Location ../../}
-function .... {Set-Location ../../../}
-function ..... {Set-Location ../../../../}
-function ...... {Set-Location ../../../../../}
-function reset {Clear-Host; pwsh.exe -nologo}
-function Uninstall-AllOutdated {Get-InstalledModule | ForEach-Object {
-  $CurrentVersion = $PSItem.Version
-  Get-InstalledModule -Name $PSItem.Name -AllVersions | Where-Object -Property Version -LT -Value $CurrentVersion
-} | Uninstall-Module -Verbose}
+function rmrf { Remove-Item -Recurse -Force $args }
+function la { Get-ChildItem -Force }
+function lw { Get-ChildItem | Format-Wide }
+function lo { Get-ChildItem | Format-Wide -Column 3 }
+function qrcode { curl qrcode.show/$args }
+function ~ { Set-Location ~ }
+function .. { Set-Location ../ }
+function ... { Set-Location ../../ }
+function .... { Set-Location ../../../ }
+function ..... { Set-Location ../../../../ }
+function ...... { Set-Location ../../../../../ }
+function reset { Clear-Host; pwsh.exe -nologo }
+function Uninstall-AllOutdated {
+  Get-InstalledModule | ForEach-Object {
+    $CurrentVersion = $PSItem.Version
+    Get-InstalledModule -Name $PSItem.Name -AllVersions | Where-Object -Property Version -LT -Value $CurrentVersion
+  } | Uninstall-Module -Verbose
+}
 Set-Alias -Name ytdl -Value youtube-dl.exe
-function yt-dlp-best {yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio" $args}
-function yt-dlp-mp3 {yt-dlp --extract-audio -f bestaudio[ext=mp3] --no-playlist $args}
-function yt-dlp-music {yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 --embed-thumbnail -o "%(title)s.%(ext)s" $args}
+function yt-dlp-best { yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio" $args }
+function yt-dlp-mp3 { yt-dlp --extract-audio -f bestaudio[ext=mp3] --no-playlist $args }
+function yt-dlp-music { yt-dlp --extract-audio --audio-format mp3 --audio-quality 0 --embed-thumbnail -o "%(title)s.%(ext)s" $args }
 
 # Custom Functions
 function wheater {
-	param ($param1)
-	Invoke-RestMethod http://wttr.in/$param1
+  param ($param1)
+  Invoke-RestMethod http://wttr.in/$param1
 }
-function ip {'Private Adress: ' + (Get-NetIPAddress | Where-Object {$_.AddressFamily -eq 'IPv4' -and $_.PrefixOrigin -eq 'Dhcp' }).IPAddress; 'Public Adress: ' + (curl -s ifconfig.me)}
+function ip { 'Private Adress: ' + (Get-NetIPAddress | Where-Object { $_.AddressFamily -eq 'IPv4' -and $_.PrefixOrigin -eq 'Dhcp' }).IPAddress; 'Public Adress: ' + (curl -s ifconfig.me) }
 function gle {
-	for ( $i = 0; $i -lt $args.count; $i++ ) {
-	$search = $search + $($args[$i]) + "+"
-	} 
-	Start-Process "https://www.google.com/search?q=$search" 
+  for ( $i = 0; $i -lt $args.count; $i++ ) {
+    $search = $search + $($args[$i]) + "+"
+  } 
+  Start-Process "https://www.google.com/search?q=$search" 
 }
 function faceit {
   param (
     [Parameter(Mandatory)]
-    [ValidateSet('on','off')]
+    [ValidateSet('on', 'off')]
     [string]$state
   )
   if ($on -eq $true) {
@@ -68,7 +71,7 @@ function faceit {
     Write-Host "Use" -NoNewLine
     Write-Host " shutdown.exe /a " -Foreground Green -NoNewLine
     Write-Host "to stop restart"
-    }
+  }
   else {
     Write-Host "Enabling Hyper-V" -Foreground Yellow
     bcdedit /set hypervisorlaunchtype auto
@@ -81,3 +84,5 @@ function faceit {
     Write-Host "to stop restart"
   }
 }
+# Enable-PoshTooltips
+# Enable-PoshTransientPrompt
