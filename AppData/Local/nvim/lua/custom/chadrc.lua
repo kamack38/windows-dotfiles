4fa107e79d79923379ee74aa5591b48aab6e10f6
kamack38 <64226248+kamack38@users.noreply.github.com>
2022-02-26 16:45:22 +0100

    🔧 conf: Remove .npmrc & Update .prettierrc, chadrc.lua, mpv.conf and VSC settings

--- AppData/Local/nvim/lua/custom/chadrc.lua
+++ AppData/Local/nvim/lua/custom/chadrc.lua
@@ -1,17 +1,5 @@
--- IMPORTANT NOTE : This is the user config, can be edited. Will be preserved if updated with internal updater
--- This file is for NvChad options & tools, custom settings are split between here and 'lua/custom/init.lua'
-
 local M = {}
 local opt = vim.opt
--- NOTE: To use this, make a copy with `cp example_chadrc.lua chadrc.lua`
---------------------------------------------------------------------
-
--- To use this file, copy the structure of `core/default_config.lua`,
--- examples of setting relative number & changing theme:
-
--- M.options = {
---    relativenumber = true,
--- }
 
 opt.shell = 'pwsh -nol'
 opt.shellcmdflag = '-nop -c'
@@ -29,19 +17,9 @@ local userPlugins = require "custom.plugins" -- path to table
 
 M.plugins = {
    status = {
-      blankline = true, -- indentline stuff
-      bufferline = true, -- manage and preview opened buffers
       colorizer = true, -- color RGB, HEX, CSS, NAME color codes
-      comment = true, -- easily (un)comment code, language aware
       dashboard = true,
-      better_escape = true, -- map to <ESC> with no lag
-      feline = true, -- statusline
-      gitsigns = true,
-      lspsignature = true, -- lsp enhancements
-      vim_matchup = true, -- improved matchit
-      cmp = true,
-      nvimtree = true,
-      autopairs = true,
+      snippets = true,
    },
    
    options = {
