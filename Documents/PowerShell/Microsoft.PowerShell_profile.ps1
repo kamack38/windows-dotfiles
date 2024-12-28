0f7623461eb4c0e28eac85aaac748045a079c6b9
kamack38 <64226248+kamack38@users.noreply.github.com>
2024-12-28 23:08:25 +0100

    feat: 🎸 Update PowerShell config

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,6 @@
 # Modules
-Import-Module posh-git
-Import-Module npm-completion
-Import-Module nvm-completion
-Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+Import-Module posh-git, npm-completion, nvm-completion, "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1", Terminal-Icons, PSReadLine
 $env:POSH_GIT_ENABLED = $true
-Import-Module Terminal-Icons
-Import-Module PSReadLine
 Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadLineOption -PredictionSource History
@@ -16,6 +11,7 @@ Set-PSReadLineOption -EditMode Windows
 Set-Alias -Name exp -Value C:\Windows\explorer.exe
 Set-Alias -Name List-Module -Value Get-InstalledModule
 Set-Alias -Name choco -Value choco.exe
+Set-Alias -Name n -Value nvim.exe
 Set-Alias -Name code -Value code.cmd
 Set-Alias -Name node -Value node.exe
 Set-Alias -Name firefox -Value "C:\Program Files\Firefox Developer Edition\firefox.exe"
@@ -47,33 +43,5 @@ function weather {
   Invoke-RestMethod http://wttr.in/$param1
 }
 function ip { 'Private Adress: ' + (Get-NetIPAddress | Where-Object { $_.AddressFamily -eq 'IPv4' -and $_.PrefixOrigin -eq 'Dhcp' }).IPAddress; 'Public Adress: ' + (curl -s ifconfig.me) }
-function faceit {
-  param (
-    [Parameter(Mandatory)]
-    [ValidateSet('on', 'off')]
-    [string]$state
-  )
-  if ($on -eq $true) {
-    Write-Host "Disabling Hyper-V" -Foreground Yellow
-    bcdedit /set hypervisorlaunchtype off
-    reg add "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity" /v "Enabled" /t REG_DWORD /d 0 /f
-    Write-Host "Restarting Computer..." -Foreground Yellow
-    shutdown.exe /t 20 /r
-    Write-Host "Your computer will be restarted in 20 sceonds." -Foreground Red
-    Write-Host "Use" -NoNewLine
-    Write-Host " shutdown.exe /a " -Foreground Green -NoNewLine
-    Write-Host "to stop restart"
-  }
-  else {
-    Write-Host "Enabling Hyper-V" -Foreground Yellow
-    bcdedit /set hypervisorlaunchtype auto
-    reg add "HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity" /v "Enabled" /t REG_DWORD /d 1 /f
-    Write-Host "Restarting Computer..."
-    shutdown.exe /t 20 /r
-    Write-Host "Your computer will be restarted in 20 sceonds." -Foreground Red
-    Write-Host "Use" -NoNewLine
-    Write-Host " shutdown.exe /a " -Foreground Green -NoNewLine
-    Write-Host "to stop restart"
-  }
-}
+
 oh-my-posh --init --shell pwsh --config ~/.config/oh-my-posh/kamack.omp.json | Invoke-Expression
